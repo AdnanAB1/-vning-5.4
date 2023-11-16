1e8a0e01dbec519da6dc4b14e46042ce63cb9793
Adnan <adnan.albarazi.edu@rattvik.se>
2023-11-16 14:44:10 +0100

    Initial commit

---
+++ Övning 5.4/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning_5._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int talA = int.Parse( tbxA.Text );
+            int talB = int.Parse( tbxB.Text );
+            int talC = int.Parse( tbxC.Text );
+            bool ordning = talA <= talB && talB <= talC; //här talar vi att talA ska vara mindre eller lika med talB och talB ska vara mindre eller lika med talC
+            lblSvar.Text = " Talen är i ordningsföljd: " + ordning;
+        }
+    }
+}
